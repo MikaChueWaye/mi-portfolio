b05436725165acf324619d5e6dc56d4fed3eb871
MikaChueWaye <mika.chuewaye@gmail.com>
2024-04-11 07:34:37 +0200

    feat: add new WelcomePage

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -12,10 +12,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/homeVue">HomeVue</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
+  </header> -->
 
   <RouterView />
 </template>
---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="centered-text">
+    <h1>What are you doing here? There is nothing to see there... yet...</h1>
+    <p>Shoo! Shoo!</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .centered-text {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>

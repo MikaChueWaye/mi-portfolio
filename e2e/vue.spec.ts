5ab06ddd9e9198dd20c0732fd08f8fb55e294fcf
MikaChueWaye <mika.chuewaye@gmail.com>
2024-04-17 19:44:39 +0200

    feat: add small tests

--- e2e/vue.spec.ts
+++ e2e/vue.spec.ts
@@ -2,9 +2,19 @@ import { test, expect } from '@playwright/test'
 
 // See here how to get started:
 // https://playwright.dev/docs/intro
-test('visits the app root url', async ({ page }) => {
+// test('visits the app root url', async ({ page }) => {
+//   await page.goto('/')
+//   await expect(page.locator('div.centered-text > h1')).toHaveText(
+//     'What are you doing here? There is nothing to see there... yet...'
+//   )
+// })
+
+test('#app present on root url', async ({ page }) => {
   await page.goto('/')
-  await expect(page.locator('div.centered-text > h1')).toHaveText(
-    'What are you doing here? There is nothing to see there... yet...'
-  )
+  await expect(page.locator('#app')).toBeDefined()
+})
+
+test('#trailer present on root url', async ({ page }) => {
+  await page.goto('/')
+  await expect(page.locator('#trailer')).toBeDefined()
 })
